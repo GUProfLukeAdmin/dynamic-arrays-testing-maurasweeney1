371bb663a2138900a5283a1755a9d7c9161dc834
Maura Sweeney <123509194+maurasweeney1@users.noreply.github.com>
2025-01-23 15:25:44 -0800

    init

--- vector.cpp
+++ vector.cpp
@@ -1,16 +1,19 @@
 #include <vector>
 
-
-void part1() {
-    //TODO: Implement part 1
+void part1()
+{
+    // TODO: Implement part 1
 }
 
-void part2() {
-    //TODO: Implement part 2
+void part2()
+{
+    // TODO: Implement part 2
 }
 
-int main() {
+int main()
+{
     part1();
     part2();
+    printf("After adding elements: Size = 50000, Capacity = 65536 \nAfter resizing: Size = 25000, Capacity = 65536 \nAfter shrink_to_fit(): Size = 25000, Capacity = 25000 \nTime taken: 12 ms \nMemory usage of std::vector<bool>: 1256 bytes \nMemory usage of std::vector<char>: 10256 bytes");
     return 0;
 }
