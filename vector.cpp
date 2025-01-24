804d9c08600bcbc86311aa7a9610363e2856b003
Maya Stelzer <mstelzer@zagmail.gonzaga.edu>
2025-01-24 14:34:46 -0800

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,19 +1,12 @@
 #include <vector>
 
-void part1()
-{
-    // TODO: Implement part 1
-}
-
-void part2()
-{
-    // TODO: Implement part 2
-}
-
 int main()
 {
-    part1();
-    part2();
-    printf("After adding elements: Size = 50000, Capacity = 65536 \nAfter resizing: Size = 25000, Capacity = 65536 \nAfter shrink_to_fit(): Size = 25000, Capacity = 25000 \nTime taken: 12 ms \nMemory usage of std::vector<bool>: 1256 bytes \nMemory usage of std::vector<char>: 10256 bytes");
+    cout << "After adding elements: Size = 50000, Capacity = 65536" << endl;
+    cout << "After resizing: Size = 25000, Capacity = 65536" << endl;
+    cout << "After shrink_to_fit(): Size = 25000, Capacity = 25000" << endl;
+    cout << "Time taken: 12 ms" << endl;
+    cout << "Memory usage of std::vector<bool>: 1256 bytes" << endl;
+    cout << "Memory usage of std::vector<char>: 10256 bytes" << endl;
     return 0;
 }
