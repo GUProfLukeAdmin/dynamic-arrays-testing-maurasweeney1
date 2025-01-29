efeeb45e3882f8751642fc2041be9983b07b4205
Maya Stelzer <mstelzer@zagmail.gonzaga.edu>
2025-01-28 17:02:53 -0800

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -4,21 +4,14 @@
 
 void part1() {
     std::vector<int> v;
-    auto start = std::chrono::high_resolution_clock::now();
     for (int i = 0; i < 50000; ++i) {
         v.push_back(i);
     }
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end - start;
     std::cout << "After adding elements: Size = " << v.size() << ", Capacity = " << v.capacity() << std::endl;
     v.resize(v.size() / 2);
     std::cout << "After resizing: Size = " << v.size() << ", Capacity = " << v.capacity() << std::endl;
     v.shrink_to_fit();
     std::cout << "After shrink_to_fit(): Size = " << v.size() << ", Capacity = " << v.capacity() << std::endl;
-
-    auto time = std::chrono::high_resolution_clock::now() - start;
-    std::chrono::duration<double> total_duration = time;
-    std::cout << "Time taken: " << total_duration.count() * 1000 << " ms" << std::endl;
 }
 
 void part2() {
@@ -27,7 +20,6 @@ void part2() {
     size_t boolVectorMemory = boolVector.capacity() / 8;
     size_t charVectorMemory = charVector.capacity();
 
-    
     std::cout << "Memory usage of std::vector<bool>: " << boolVectorMemory << " bytes" << std::endl;
     std::cout << "Memory usage of std::vector<char>: " << charVectorMemory << " bytes" << std::endl;
 }
